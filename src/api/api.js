ef6d1f7b7b32c2ea5033c0deb626355c1547bc73
Aleksandr Barmin <albarmin4549@gmail.com>
2020-06-14 16:58:43 +0300

    Create a React app and publish it to GitHub Pages

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const getWeather = id => {
   return axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?id=${id}&lang=ru&units=metric&APPID=04396a6162f4cbfb469bf546ebfc29cb`
+    `https://api.openweathermap.org/data/2.5/weather?id=${id}&lang=ru&units=metric&APPID=04396a6162f4cbfb469bf546ebfc29cb`
   )
 }
