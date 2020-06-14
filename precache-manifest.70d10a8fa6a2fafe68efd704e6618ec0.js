e6e13459938af51530305718ef292c73e6722dde
Aleksandr Barmin <albarmin4549@gmail.com>
2020-06-14 15:55:50 +0300

    Updates

---
+++ precache-manifest.70d10a8fa6a2fafe68efd704e6618ec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bc73eb5855955347c5708f63cf433c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d9fbe3d18cc70340b274",
+    "url": "/weather-app/static/css/main.2950434c.chunk.css"
+  },
+  {
+    "revision": "09d4b3265841075fd7bc",
+    "url": "/weather-app/static/js/2.28631723.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/weather-app/static/js/2.28631723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fbe3d18cc70340b274",
+    "url": "/weather-app/static/js/main.95359eb2.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  },
+  {
+    "revision": "28fc8b109de62810d17b64d3b826b1bc",
+    "url": "/weather-app/static/media/logo.28fc8b10.png"
+  }
+]);
